48584fa5bc79fa3dcee18ae479bc7af708829a41
Patrick Oliveira <patrick.edulivre@gmail.com>
2023-06-04 19:06:22 -0300

    test: ensure RemoteAuthentication returns an Account on success

--- other/clean-arch-login/src/Auth/data/usecases/remote-auth/remote-auth.spec.ts
+++ other/clean-arch-login/src/Auth/data/usecases/remote-auth/remote-auth.spec.ts
@@ -1,7 +1,7 @@
 import { RemoteAuthentication } from "./remote-auth"
 import { HttpPostClientSpy } from "../../test/mock-http-client"
 import { faker } from "@faker-js/faker"
-import { mockAuthentication } from "@/Auth/domain/test/mock-authentication"
+import { mockAccountModel, mockAuthentication } from "@/Auth/domain/test/mock-authentication"
 import { InvalidCredentialsError } from "@/Auth/domain/erros/invalid-credentials-error"
 import { HttpStatusCode } from "../../protocols/http/http-response"
 import { UnexpectedError } from "@/Auth/domain/erros/unexpected-error"
@@ -71,5 +71,17 @@ describe('RemoteAuthentication', () => {
         }
         const promise = sut.auth(mockAuthentication())
         await expect(promise).rejects.toThrow(new UnexpectedError())
+    })
+
+
+    test('Shoud return an AccountModel if HttpPostClient returns returns 200', async () => {
+        const { sut, httpPostClientSpy } = makeSut()
+        const httpResult = mockAccountModel()
+        httpPostClientSpy.response = {
+            statusCode: HttpStatusCode.ok,
+            body: httpResult
+        }
+        const account = await sut.auth(mockAuthentication())
+        await expect(account).toEqual(httpResult)
     })
 })
--- other/clean-arch-login/src/Auth/domain/test/mock-authentication.ts
+++ other/clean-arch-login/src/Auth/domain/test/mock-authentication.ts
@@ -1,7 +1,12 @@
+import { AccountModel } from "../models/account-model";
 import { AuthParams } from "../usecases/email-passord";
 import { faker } from "@faker-js/faker";
 
 export const mockAuthentication = (): AuthParams => ({
     email: faker.internet.email(),
     passoword: faker.internet.password()
+})
+
+export const mockAccountModel = (): AccountModel => ({
+    accessToken: faker.string.uuid(),
 })
