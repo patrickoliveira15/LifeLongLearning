e51f0b5880a64a08d31f91f5ee3389c770acc2ff
Patrick Oliveira <patrick.edulivre@gmail.com>
2023-06-04 19:05:26 -0300

    feat: ensure RemoteAuthentication return an Account on success

--- other/clean-arch-login/src/Auth/data/usecases/remote-auth/remote-auth.ts
+++ other/clean-arch-login/src/Auth/data/usecases/remote-auth/remote-auth.ts
@@ -1,24 +1,24 @@
-import { AuthParams } from "@/Auth/domain/usecases/email-passord"
+import { AuthParams, EmailAuthentication } from "@/Auth/domain/usecases/email-passord"
 import { HttpPostClient } from "../../protocols/http/http-post-client"
 import { HttpStatusCode } from "../../protocols/http/http-response"
 import { InvalidCredentialsError } from "@/Auth/domain/erros/invalid-credentials-error"
 import { UnexpectedError } from "@/Auth/domain/erros/unexpected-error"
 import { AccountModel } from "@/Auth/domain/models/account-model"
 
-export class RemoteAuthentication {
+export class RemoteAuthentication implements EmailAuthentication {
     constructor (
         private readonly url: string,
         private readonly httpPostClient: HttpPostClient<AuthParams, AccountModel>
         ) {}
 
-    async auth (params: AuthParams): Promise<void> {
+    async auth (params: AuthParams): Promise<AccountModel> {
         const httpResponse = await this.httpPostClient.post({
             url: this.url,
             body: params
         })
 
         switch (httpResponse.statusCode) {
-            case HttpStatusCode.ok: break
+            case HttpStatusCode.ok: return httpResponse.body
             case HttpStatusCode.unathorized: throw new InvalidCredentialsError()
             default: throw new UnexpectedError()
         }
