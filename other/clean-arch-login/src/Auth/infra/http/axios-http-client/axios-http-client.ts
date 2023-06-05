e1dbc2850e8b62eb33dc510aeee1e960e7361563
Patrick Oliveira <patrick.edulivre@gmail.com>
2023-06-05 11:58:09 -0300

    feat: implements axios client

---
+++ other/clean-arch-login/src/Auth/infra/http/axios-http-client/axios-http-client.ts
@@ -0,0 +1,13 @@
+import { HttpPostClient, HttpPostParams } from "@/Auth/data/protocols/http/http-post-client";
+import { HttpResponse } from "@/Auth/data/protocols/http/http-response";
+import axios from "axios";
+
+export class AxiosHttpClient implements HttpPostClient<any, any> {
+    async post (params: HttpPostParams<any>): Promise<HttpResponse<any>> {
+        const response = await axios.post(params.url, params.body)
+        return {
+            statusCode: response.status,
+            body: response.data
+        }
+    }
+}
