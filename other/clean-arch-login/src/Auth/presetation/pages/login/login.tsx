ac8d16f7c4a0da6e4871993e41bdc9359d059071
Patrick Oliveira <patrick.edulivre@gmail.com>
2023-06-17 12:58:53 -0300

    feat: add login page

---
+++ other/clean-arch-login/src/Auth/presetation/pages/login/login.tsx
@@ -0,0 +1,57 @@
+'use client'
+
+import { useState } from 'react';
+
+export default function Home() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Lógica de autenticação aqui
+  };
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <div className="flex justify-center items-center p-12">
+      <form className="w-full max-w-sm p-4" onSubmit={handleSubmit}>
+        <h1 className="text-2xl font-bold mb-4">Login</h1>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+            Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Sign In
+          </button>
+        </div>
+      </form>
+    </div>
+    </main>
+  )
+}
